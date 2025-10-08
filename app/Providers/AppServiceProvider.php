af89cb51de054ef4e142fc8944147213dc12d66d
banban <kjy0939@gmail.com>
2025-10-08 12:40:36 +0900

    mixed contents error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // HTTPS 프로덕션 환경에서 강제 적용
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // 프록시 신뢰 설정 (Nginx/Apache 뒤에서 실행 시)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
