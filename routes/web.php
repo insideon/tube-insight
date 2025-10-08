c027a4235f760e3200beb15d71d43c57be7c9660
banban <kjy0939@gmail.com>
2025-10-08 09:08:25 +0900

    redirect to admin

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::redirect('/', '/admin')->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
